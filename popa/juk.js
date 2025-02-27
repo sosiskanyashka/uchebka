f4c4b59800fed56542053091ed1e8b33d9bd0c98
sosiskanyashka <nymfyoda@gmail.com>
2025-02-27 12:51:50 +0700

    Delete popa/juk.js

--- popa/juk.js
+++
@@ -1,9 +0,0 @@
-// Пример: всплывающее окно при нажатии на кнопку "Отправить"
-const contactForm = document.querySelector('#contacts form');
-
-if (contactForm) {
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Предотвращаем отправку формы
-        alert('Сообщение отправлено!'); // Простое всплывающее окно
-    });
-}
